6787ae85254eb6c5bbff79c1c688d3f4aab56de2
Sebastian Naranjo <sebas2013n@gmail.com>
2022-04-16 14:41:50 -0500

    Reestructura

--- README.md
+++
@@ -1,1 +0,0 @@
-# Chat-con-Socket.IO
--- sockets/sever.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-const http = require('http');
-
-const server = http.createServer(app);
-server.listen(3000);
